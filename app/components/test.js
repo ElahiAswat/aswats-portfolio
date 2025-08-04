e8f1563017819dc46effee3e0e4e7074480737d6
Aswat <aswat.mohammed@northsouth.edu>
2025-08-04 15:46:49 +0600

    shushto

--- app/components/test.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function Test() {
-  return (
-    <div style={{ color: '#CFFF04', background: '#000', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem' }}>
-      Test page loaded successfully!
-    </div>
-  );
-}
