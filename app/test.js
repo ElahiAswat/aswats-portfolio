f332409197ccd2f07dcb35db9b70732dc7a2fc56
Aswat <aswat.mohammed@northsouth.edu>
2025-08-04 15:14:50 +0600

    test

---
+++ app/test.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Test() {
+  return (
+    <div style={{ color: '#CFFF04', background: '#000', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem' }}>
+      Test page loaded successfully!
+    </div>
+  );
+}
